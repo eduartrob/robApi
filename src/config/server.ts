3f0e572a6b75f805dfeec984e8b352df61e1e845
Esduardo <eduartrob@gmail.com>
2025-07-11 10:19:10 -0600

    change-url-post-image

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,10 +3,11 @@ import { app } from '../app';
 
 const startServer = () => {
   const server = http.createServer(app);
-  const port = process.env.PORT;
+  const port = Number(process.env.PORT) || 3000; // Asegura que el puerto sea un número
+  const host = '0.0.0.0'; // O '127.0.0.1' para localhost, o la IP de tu máquina (ej. '192.168.1.5')
 
-  server.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+  server.listen(port, host, () => { // CAMBIO AQUÍ: Añade 'host'
+    console.log(`Server running on http://${host}:${port}`);
   });
 };
 
